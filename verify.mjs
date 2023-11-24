8508a53d5986b274716ad939451442283224c5c8
Yash <67926590+Yash094@users.noreply.github.com>
2023-11-25 00:14:26 +0530

    Update: Using clientId/secretKey at ThirdwebSDK initialization

--- verify.mjs
+++ verify.mjs
@@ -1,7 +1,11 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { CronosBeta } from "@thirdweb-dev/chains" // Chain the contract you want to verify is on
 
-const sdk = new ThirdwebSDK(CronosBeta)
+const sdk = new ThirdwebSDK(CronosBeta, {
+  clientId: "YOUR_CLIENT_ID", // Use client id if using on the client side, get it from dashboard settings
+  secretKey: "YOUR_SECRET_KEY", // Use secret key if using on the server, get it from dashboard settings
+});
+
 const explorerAPIUrl = ""; // e.g. https://api.etherscan.io/api
 const explorerAPIKey = ""; // Generate API key on the explorer
 
